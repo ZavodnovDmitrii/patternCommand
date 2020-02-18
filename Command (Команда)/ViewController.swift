25f7c294ff1b0511f8a03353b7f1b5b508e17ec2
Dmitrii Zavodnov <dmitrii.zavodnov@mail.ru>
2020-02-18 13:48:40 +0300

    create pattern command

--- Command (Команда)/ViewController.swift
+++ Command (Команда)/ViewController.swift
@@ -1,20 +1,90 @@
-//
-//  ViewController.swift
-//  Command (Команда)
-//
-//  Created by Дмитрий Заводнов on 2/18/20.
-//  Copyright © 2020 Дмитрий Заводнов. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var text: UITextField!
+    let commandExecuter = CommandExecuter()
+    var undoCom: UndoCommand?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        text.delegate = self
     }
+    
+    @IBAction func backButton(_ sender: Any) {
+        text.text = commandExecuter.undoLast()
+    }
+}
 
+//MARK: - Delegate
+extension ViewController : UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if let text = textField.text {
+            undoCom = UndoCommand(argument: text)
+            commandExecuter.addCommand(addCommand: undoCom!)
+            commandExecuter.addition(string)
+        }
+        return true
+    }
+}
 
+//MARK: - Protocol
+protocol BaseCommandProtocol {
+    func undo() -> String
+    func addition(_ str: String)
+}
+
+//MARK: - UndoCommand
+class UndoCommand: BaseCommandProtocol {
+    private var originalString: String
+    private var currentString: String
+    
+    init(argument: String) {
+        self.currentString = argument
+        self.originalString = argument
+    }
+    
+    func undo() -> String {
+        if currentString.count != 0 {
+        currentString.removeLast()
+        } else { return ""}
+        printCurrentString()
+        return currentString
+    }
+    
+    func addition(_ str: String) {
+        currentString.append(str)
+        printCurrentString()
+    }
+    
+    func printCurrentString() {
+        print(currentString)
+    }
+}
+
+//MARK: - CommandExecuter
+class CommandExecuter {
+    private var arrayOfCommand = [BaseCommandProtocol]()
+    
+    func addCommand(addCommand: BaseCommandProtocol) {
+        arrayOfCommand.append(addCommand)
+    }
+    
+    func addition(_ str: String) {
+        for command in arrayOfCommand {
+            command.addition(str)
+        }
+    }
+    
+    func undoLast() -> String {
+        var title = String()
+        for command in arrayOfCommand {
+            title = command.undo()
+        }
+        return title
+    }
 }
 
